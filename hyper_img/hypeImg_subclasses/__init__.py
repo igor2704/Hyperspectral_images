15a0a8a47e5ab00adf1c95ed85fa76af093fc374
Igor <107305421+igor2704@users.noreply.github.com>
2023-08-01 15:59:37 +0700

    Delete hyper_img directory

--- hyper_img/hypeImg_subclasses/__init__.py
+++
@@ -1,4 +0,0 @@
-from hyper_img.hypeImg_subclasses.table_hyperImg import TableHyperImg
-
-
-__all__ = ['TableHyperImg']
--- hyper_img/hyperImg_segmenter/__init__.py
+++
@@ -1,4 +0,0 @@
-from hyper_img.hyperImg_segmenter.segmenter import Segmenter
-from hyper_img.hyperImg_segmenter.segmenter_subclasses.plain_cv2_segmenter import PlainCv2Segmenter
-
-__all__ = ['Segmenter', 'PlainCv2Segmenter']
